494419d6c5c8555169df202e0a782c9d2c94f2fd
Thomas Bracher <thchblog@gmail.com>
2016-02-23 21:22:42 +0100

    add saucelabs support

--- karma.conf.js
+++ karma.conf.js
@@ -2,66 +2,61 @@
 // Generated on Tue Dec 29 2015 22:20:18 GMT+0100 (CET)
 
 module.exports = function(config) {
-  config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
+  var configuration = {
     basePath: '',
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha', 'chai'],
-
-    // list of files / patterns to load in the browser
-    files: [
-      'hyacinth.js',
-      'test/**/*.test.js'
-    ],
-
-    // list of files to exclude
-    exclude: [
-      '**/*.swp'
-    ],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    files: [ 'hyacinth.js', 'test/**/*.test.js' ],
+    exclude: [ '**/*.swp' ],
+    preprocessors: { },
     reporters: ['progress'],
-
-    // web server port
     port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Firefox'],
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
+    concurrency: Infinity,,
+    client: { captureConsole: true, mocha: { bail: true } },
+  };
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity,
-
-    client: {
-      captureConsole: true,
-      mocha: {
-        bail: true
+  if (process.env.SAUCE_USERNAME && process.env.SAUCE_ACCESS_KEY) {
+    var customLaunchers = {
+      'SL_Chrome': {
+        base: 'SauceLabs',
+        platform: 'OS X 10.11',
+        browserName: 'chrome',
+        customData: {
+          awesome: true
+        }
+      },
+      'SL_Firefox': {
+        base: 'SauceLabs',
+        platform: 'OS X 10.11',
+        browserName: 'firefox'
+      },
+      'SL_Edge': {
+        base: 'SauceLabs',
+        platform: 'Windows 10',
+        browserName: 'microsoftedge'
       }
-    }
-  })
+    };
+
+    configuration.reporters.push('saucelabs');
+    configuration.sauceLabs = {
+      testName: 'Karma and Sauce Labs demo',
+      recordScreenshots: false,
+      connectOptions: {
+        port: 5757,
+        logfile: 'sauce_connect.log'
+      },
+      public: 'public'
+    };
+    // Increase timeout in case connection in CI is slow
+    configuration.captureTimeout = 120000;
+    configuration.customLaunchers = customLaunchers;
+    configuration.browsers = Object.keys(customLaunchers);
+  }
+
+  config.set(configuration);
 }
